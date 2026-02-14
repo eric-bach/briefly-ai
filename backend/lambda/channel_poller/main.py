354e0892e3d44e52e2aa0affdbd8792825a81a77
Eric Bach <bach.eric@gmail.com>
2026-02-13 19:07:12 -0700

    Update messages

--- backend/lambda/channel_poller/main.py
+++ backend/lambda/channel_poller/main.py
@@ -389,17 +389,15 @@ def invoke_agent(video_url, instructions, channel_title="Briefly", video_title="
     logger.info(f"Summary content: {summary}")
 
     # Check for "apology" messages indicating transcript is unavailable
-    # fail_phrases = [
-    #     "I apologize",
-    #     "I couldn't retrieve the transcript",
-    #     "subtitles or transcripts are disabled for this video",
-    #     "transcripts are disabled for this video",
-    #     "I wasn't able to retrieve the transcript",
-    #     "it seems that subtitles are disabled", 
-    #     "I cannot access the transcript"
-    # ]
     fail_phrases = [
         "The system returned an error"
+        "I apologize",
+        "I couldn't retrieve the transcript",
+        "subtitles or transcripts are disabled for this video",
+        "transcripts are disabled for this video",
+        "I wasn't able to retrieve the transcript",
+        "it seems that subtitles are disabled", 
+        "I cannot access the transcript"
     ]
     
     for phrase in fail_phrases:
